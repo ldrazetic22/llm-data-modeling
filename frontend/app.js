651c67cdde12b7c85976d7fe2ad565934bf28a03
Luka <ldrazetic22@student.foi.hr>
2026-08-20 20:43:10 +0200

    Mermaid link fix

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs';
-import elkLayouts from 'https://cdn.jsdelivr.net/npm/@mermaid-js/layout-elk@1/dist/mermaid-layout-elk.esm.min.mjs';
+import elkLayouts from 'https://cdn.jsdelivr.net/npm/@mermaid-js/layout-elk@0/dist/mermaid-layout-elk.esm.min.mjs';
 
 mermaid.registerLayoutLoaders(elkLayouts);
 mermaid.initialize({ startOnLoad: false, layout: 'elk' });
